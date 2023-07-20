8e5fe3200385867ce909e18913b7759a7f05e1bb
Aryamanraj <20cs3011@rgipt.ac.in>
2023-07-21 02:03:10 +0530

    Main listening and sending files

--- src/mqttListener.js
+++ src/mqttListener.js
@@ -1,34 +1,100 @@
 const mqtt = require('mqtt');
+const fs = require('fs');
+const path = require('path');
 
-// MQTT broker URL
-const brokerUrl = 'mqtt://192.168.1.5:1888'; // Replace with your broker's IP or domain if it's hosted elsewhere
+// Get MQTT broker IP, topic, and port from command-line arguments
+const args = process.argv.slice(2);
+if (args.length < 3) {
+  console.log('Usage: node subscriber.js <broker_ip> <port> <topic>');
+  process.exit(1);
+}
 
-// MQTT topic to publish the message to
-const topic = 'test'; // Replace 'test' with the topic you want to subscribe to
+const brokerIp = args[0];
+const port = parseInt(args[1]);
+const topic = args[2];
+
+// Create a write stream to log outputs to stderr.log
+const logFilePath = path.join(__dirname, '../logfiles/', 'stderrListener.log');
+const logStream = fs.createWriteStream(logFilePath, { flags: 'a' });
+
+// Create a write stream to write received messages directly to received.csv
+const receivedFilePath = path.join(__dirname, '../receivedFiles',  '/received.csv');
+const receivedStream = fs.createWriteStream(receivedFilePath, { flags: 'a' });
+
+// Function to check if the received.csv file is empty
+function isFileEmpty(filePath) {
+  try {
+    const stats = fs.statSync(filePath);
+    return stats.size === 0;
+  } catch (err) {
+    return true; // File doesn't exist or an error occurred while accessing it
+  }
+}
+
+// Flag to track if the received.csv file is empty
+let isFirstLine = isFileEmpty(receivedFilePath);
+
+// Function to convert a JavaScript object to a CSV string
+function convertToCsv(dataObject) {
+  const dataArray = [];
+  for (const key in dataObject) {
+    dataArray.push(dataObject[key]);
+  }
+  return dataArray.join(',') + '\n';
+}
 
 // Connect to the MQTT broker
-const client = mqtt.connect(brokerUrl);
-
-// Callback to execute when the client is connected
+const client = mqtt.connect(`mqtt://${brokerIp}:${port}`);
 client.on('connect', () => {
-  console.log('Connected to MQTT broker');
-
-  // Subscribe to the topic
+  logStream.write('Connected to MQTT broker\n');
   client.subscribe(topic, (err) => {
     if (err) {
-      console.error('Error subscribing to topic:', err);
-    } else {
-      console.log(`Subscribed to topic: ${topic}`);
+      logStream.write(`Error subscribing to topic: ${err}\n`);
     }
   });
 });
 
-// Callback to execute when a message is received
+// Callback to handle incoming messages
 client.on('message', (topic, message) => {
-  console.log(`Received message on topic ${topic}: ${message.toString()}`);
+  try {
+    const jsonMessage = JSON.parse(message.toString());
+
+    // Add timestampAtAggregator field with the current timestamp in seconds
+    jsonMessage.timestampAtAggregator = Math.floor(Date.now() / 1000);
+
+    // Convert the message to a CSV string
+    const csvString = convertToCsv(jsonMessage);
+
+    // Check if the received.csv file is empty and add attribute names as the first line
+    if (isFirstLine) {
+      const attributeNames = Object.keys(jsonMessage);
+      receivedStream.write(attributeNames.join(',') + '\n', (err) => {
+        if (err) {
+          logStream.write(`Error saving attribute names: ${err}\n`);
+        }
+      });
+      isFirstLine = false;
+    }
+
+    // Write the CSV string directly to received.csv
+    receivedStream.write(csvString, (err) => {
+      if (err) {
+        logStream.write(`Error saving received CSV: ${err}\n`);
+      }
+    });
+  } catch (err) {
+    logStream.write(`Error processing MQTT message: ${err}\n`);
+  }
 });
 
-// Callback to execute when the client is disconnected
+// Handle MQTT errors
+client.on('error', (err) => {
+  logStream.write(`MQTT error: ${err}\n`);
+});
+
+// Handle MQTT disconnect
 client.on('close', () => {
-  console.log('Disconnected from MQTT broker');
+  logStream.write('Disconnected from MQTT broker\n');
+  receivedStream.end(); // Close the received stream when the script is finished
+  logStream.end(); // Close the log stream when the script is finished
 });
--- src/mqttSender.js
+++ src/mqttSender.js
@@ -1,34 +1,78 @@
 const mqtt = require('mqtt');
+const fs = require('fs');
+const path = require('path');
 
-// MQTT broker URL
-const brokerUrl = 'mqtt://192.168.1.5:1888'; // Replace 'localhost' with your broker's IP or domain if it's hosted elsewhere
+// Get MQTT broker IP, topic, port, and CSV file path from command-line arguments
+const args = process.argv.slice(2);
+if (args.length < 4) {
+  console.log('Usage: node mqttSender.js <broker_ip> <port> <topic> <csv_file_path>');
+  process.exit(1);
+}
 
-// MQTT topic to publish the message to
-const topic = 'test'; // Replace 'myTopic' with the topic you want to use
+const brokerIp = args[0];
+const port = parseInt(args[1]);
+const topic = args[2];
+const csvFilePath = args[3]; // CSV file path provided as an argument
 
-// Message to publish
-const message = 'Hello, MQTT!'; // Replace with your desired message
+// Create a write stream to log the published data
+const logFilePath = path.join(__dirname, '../', 'logfiles', 'stderr.log');
+const logStream = fs.createWriteStream(logFilePath, { flags: 'a' });
 
-// Connect to the MQTT broker
-const client = mqtt.connect(brokerUrl);
-
-// Callback to execute when the client is connected
-client.on('connect', () => {
-  console.log('Connected to MQTT broker');
-
-  // Publish the message to the topic
-  client.publish(topic, message, (err) => {
+// Function to read the CSV file and send its content as a message
+function sendCsvAsMessage() {
+  fs.readFile(csvFilePath, 'utf8', (err, data) => {
     if (err) {
-      console.error('Error publishing message:', err);
-    } else {
-      console.log(`Message published: ${message}`);
-      // Disconnect the client after publishing the message
-      client.end();
+      console.error('Error reading CSV file:', err);
+      return;
     }
-  });
-});
 
-// Callback to execute when the client is disconnected
-client.on('close', () => {
-  console.log('Disconnected from MQTT broker');
-});
+    const rows = data.split('\n');
+    const validRows = rows.filter(row => row.trim() !== ''); // Remove empty rows
+    const attributes = validRows.shift().split(','); // Extract attribute names from the first row
+
+    const client = mqtt.connect(`mqtt://${brokerIp}:${port}`);
+
+    client.on('connect', () => {
+      console.log('Connected to MQTT broker');
+
+      // Publish the messages to the topic with the specified interval
+      const messageInterval = setInterval(() => {
+        if (validRows.length > 0) {
+          const row = validRows.shift().split(',');
+          const message = {};
+
+          // Create a JSON object with attribute names as keys and row values as values
+          for (let i = 0; i < attributes.length; i++) {
+            message[attributes[i]] = row[i].trim();
+          }
+
+          // Add the timestamp from the IoT device with the key "timestampFromIotDevice"
+          message.timestampFromIotDevice = Math.floor(Date.now() / 1000);
+
+          // Check if the message contains "NULL" and avoid sending it
+          if (Object.values(message).includes('NULL')) {
+            //console.log('Skipping NULL value:', message);
+            logStream.write(`Skipping NULL value: ${JSON.stringify(message)}\n`);
+          } else {
+            client.publish(topic, JSON.stringify(message), (err) => {
+              if (err) {
+                console.error('Error publishing message:', err);
+              } else {
+                //console.log('Message published:', message);
+                logStream.write(`Message published: ${JSON.stringify(message)}\n`);
+              }
+            });
+          }
+        } else {
+          clearInterval(messageInterval); // Stop the interval when all rows are processed
+          client.end(); // Disconnect the client after publishing all the messages
+          console.log('Disconnected from MQTT broker');
+          logStream.end(); // Close the log stream when the script is finished
+        }
+      }, 5000); // Interval time in milliseconds (e.g., 5000 ms = 5 seconds)
+    });
+  });
+}
+
+// Initial call to send the CSV file content as messages
+sendCsvAsMessage();
